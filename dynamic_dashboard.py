ed9e83d87e090b562290912349ff37037c5ea11c
Isaac D'Césares <dcesares.zarac@gmail.com>
2022-09-26 21:30:14 -0300

    ADD Apresentação FIX Typo in dashboard

--- dynamic_dashboard.py
+++ dynamic_dashboard.py
@@ -15,7 +15,7 @@ def fetch_data(url):
     data = gpd.read_file(url)
     return data
 
-df = gpd.read_file('Data_Result/queimadas_pantanal_2021_2021.geojson')
+df = gpd.read_file('Data_Result/queimadas_pantanal_2012_2021.geojson')
 
 st.title('Dashboard - Queimadas no Pantanal: uma análise exploratória dos últimos 10 anos no Brasil')
 
@@ -36,7 +36,7 @@ st.subheader('Mapa com as queimadas no ano selecionado')
 fig = px.choropleth_mapbox(df[df['Ano'] == ano], geojson=df[df['Ano'] == ano].geometry, locations=df[df['Ano'] == ano].index,
                             color='Ano',
                             color_continuous_scale="Viridis",
-                            range_color=(0, 12),
+                            range_color=(12, 21),
                             mapbox_style="carto-positron",
                             zoom=3, center={"lat": -15.7801, "lon": -47.9292},
                             opacity=0.5,
